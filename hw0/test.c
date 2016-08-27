688ce14885f50cf877f46032747d66dbc061ef5e
Daniel Khuu <daniel.khuu@stonybrook.edu>
2016-08-26 19:12:52 -0700

    Edited the test.c file

--- hw0/test.c
+++ hw0/test.c
@@ -6,3 +6,11 @@ int main(int argc, char *argv[]) {
 	printf("Code pulled from remote repository.\n");
 	return EXIT_SUCCESS;
 }
+
+/* Hello Students! program */
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+fprintf(stdout, "%s\n", "Hello Students!");
+return EXIT_SUCCESS;
+}

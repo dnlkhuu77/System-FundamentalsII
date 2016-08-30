a304cd4a94b101afb782671bb249e4b640e9dcf0
Daniel Khuu <daniel.khuu@stonybrook.edu>
2016-08-30 07:07:05 -0700

    Deleted the inital message.

--- hw0/test.c
+++ hw0/test.c
@@ -1,12 +1,3 @@
-// hello.c
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-	printf("Code pulled from remote repository.\n");
-	return EXIT_SUCCESS;
-}
-
 /* Hello Students! program */
 #include <stdlib.h>
 #include <stdio.h>

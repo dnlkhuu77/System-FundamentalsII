5808b7d29d7500cd2aa88787f76329b20456c4f4
Daniel Khuu <daniel.khuu@stonybrook.edu>
2016-09-01 15:26:02 -0700

    Completed the first function and Makefile function.

--- hw1/src/map_reduce.c
+++ hw1/src/map_reduce.c
@@ -2,54 +2,72 @@
 //FUNCTION INDEPENDENTLY WITH OUR OWN MAIN PROGRAM.
 #include "map_reduce.h"
 #include <string.h>
+#include <dirent.h>
 
 //Implement map_reduce.h functions here.
 int validateargs(int argc, char** argv){ //2, 3, 4 arguments
-
-	if (strcmp(argv[1], "-h") == 0){
-		printf("FUNC\tWhich operation you would like to run on the data:\n");
-		printf("ana - Analysis of various text files in a directory.\n");
-		printf("stats - Calculates stats on files which contain only numbers.\n");
-		printf("DIR\tThe directory in which the files are located.\n \n");
-		printf("Options: \n");
-		printf("-h\tPrints this help menu.\n");
-		printf("-v\tPrints the map function's results, stating this file it's from.\n");
-		return EXIT_SUCCESS; //or 0
+	if (strcmp(argv[1], "-h") == 0){ //check to see if first flag is -h
+		return 0;
 	}
-
 	if(argc < 2 || argc > 4){
-		printf("FUNC\nWhich operation you would like to run on the data:\n");
-		printf("ana - Analysis of various text files in a directory.\n");
-		printf("stats - Calculates stats on files which contain only numbers.\n");
-		printf("DIR\nThe directory in which the files are located.\n \n");
-		printf("Options: \n");
-		printf("-h\tPrints this help menu.\n");
-		printf("-v\tPrints the map function's results, stating this file it's from.\n");
-		return -1; //1
+		return -1;
 	}
 
 	if(strcmp(argv[1], "ana") == 0){
-		return 1;
-	}
-	if(strcmp(argv[1], "stats") == 0){
-		return 2;
-	}
-	if(strcmp(argv[1], "-v") == 0){
-		if(argv[2] == NULL)
+		if(argv[2] == NULL || argv[3] != NULL)
 			return -1;
-		if(strcmp(argv[2], "ana") == 0)
-			return 3;
-		if(strcmp(argv[2], "stats") == 0)
-			return 4;
+
+		DIR* dir = opendir(argv[2]);
+		if(dir){
+			closedir(dir);
+			return 1;
+		}
+		else
+			return -1;
 	}
 
-	printf("FUNC\tWhich operation you would like to run on the data:\n");
-	printf("ana - Analysis of various text files in a directory.\n");
-	printf("stats - Calculates stats on files which contain only numbers.\n");
-	printf("DIR\tThe directory in which the files are located.\n \n");
-	printf("Options: \n");
-	printf("-h\tPrints this help menu.\n");
-	printf("-v\tPrints the map function's results, stating this file it's from.\n");
+	if(strcmp(argv[1], "stats") == 0){
+		if(argv[2] == NULL || argv[3] != NULL)
+			return -1;
+
+		DIR* dir = opendir(argv[2]);
+		if(dir){
+			closedir(dir);
+			return 2;
+		}
+		else
+			return -1;
+	}
+
+	if(strcmp(argv[1], "-v") == 0){
+		if(argv[2] == NULL || argv[3] == NULL)
+			return -1;
+
+		if(strcmp(argv[2], "ana") == 0){
+			DIR* dir = opendir(argv[3]);
+				if(dir){
+					closedir(dir);
+					return 3;
+				}
+				else
+					return -1;
+		}
+
+		if(strcmp(argv[2], "stats") == 0){
+			DIR* dir = opendir(argv[3]);
+				if(dir){
+					closedir(dir);
+					return 4;
+				}
+				else
+					return -1;
+		}
+	}
+
 	return -1;
 }
-
+/*int nfiles(char* dir){
+	int files = 0;
+	DIR *ptr;
+	struct 
+}*/

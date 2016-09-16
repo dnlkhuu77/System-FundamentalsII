20e9490628c315c0cc36d3744bf2bbd10f4f8575
Daniel Khuu <daniel.khuu@stonybrook.edu>
2016-09-16 17:10:08 -0400

    HW2_P2

--- hw2/src/utfconverter.c
+++ hw2/src/utfconverter.c
@@ -1,23 +1,25 @@
 #include "utfconverter.h"
 
 int main(int argc, char** argv){
+	unsigned char buf[2];
+	int fd = 0;
+	int rv = 0;
+	Glyph* glyph= NULL;
 	/* After calling parse_args(), filename and conversion should be set. */
 	parse_args(argc, argv);
-	int fd = open(filename, O_RDONLY); 
-	unsigned char buf[2] = {0};
-	int rv = 0;
+	fd = open(filename, O_RDONLY); 
 
-	Glyph* glyph = malloc(sizeof(Glyph));
+	glyph = malloc(sizeof(Glyph));
 	
 	/* Handle BOM bytes for UTF16 specially. 
          * Read our values into the first and second elements. */
 	if((rv = read(fd, &buf[0], 1)) == 1 && (rv = read(fd, &buf[1], 1)) == 1){
 
-		if(buf[0] == 0xfe && buf[1] == 0xff){ //big
-			/*file is little endian*/
+		if(buf[0] == 0xfe && buf[1] == 0xff){
+			/*file is big endian*/
 			source = BIG; 
 		} else if(buf[0] == 0xff && buf[1] == 0xfe){
-			/*file is big endian*/
+			/*file is little endian*/
 			source = LITTLE;
 		} else {
 			/*file has no BOM*/
@@ -67,6 +69,7 @@ Glyph* swap_endianness(Glyph* glyph){
 }
 
 Glyph* fill_glyph(Glyph* glyph, unsigned char data[2], endianness end, int* fd){
+		unsigned int bits;
 
 	if(end == LITTLE){
 		glyph->bytes[0] = data[0];
@@ -76,16 +79,13 @@ Glyph* fill_glyph(Glyph* glyph, unsigned char data[2], endianness end, int* fd){
 		glyph->bytes[0] = data[1]; 
 		glyph->bytes[1] = data[0];
 	}
-
-	unsigned int bits = 0; 
+	bits = 0; 
 	bits |= ((data[FIRST] << 8 ) + data[SECOND]);
 	/* Check high surrogate pair using its special value range.*/
 
-	/*fprintf(stderr, "%08x\n", bits);*/
-
 	if(bits > 0xD800 && bits < 0xDBFF){ 
 		/*fprintf(stderr, "STUFFFFFFF\n" );*/
-		if(read(*fd, &data[SECOND], 1) == 1 && read(*fd, &data[FIRST], 1) == 1){
+		if(read(*fd, &data[FIRST], 1) == 1 && read(*fd, &data[SECOND], 1) == 1){
 			bits = 0;
 			bits |= ((data[FIRST] << 8) + data[SECOND]);
 
@@ -93,19 +93,23 @@ Glyph* fill_glyph(Glyph* glyph, unsigned char data[2], endianness end, int* fd){
 			if(bits > 0xDC00 && bits < 0xDFFF){
 				glyph->surrogate = true; 
 			} else {
-				lseek(*fd, -OFFSET, SEEK_CUR); 
-				glyph->surrogate = false;
+				printf("Invalid\n");
+				print_help();
 			}
 		}
 
-	}
+	}else
+		glyph->surrogate = false;
+
 
 	if(!glyph->surrogate){
-		glyph->bytes[THIRD] = glyph->bytes[FOURTH] |= 0;
+		glyph->bytes[THIRD] = '\0';
+		glyph->bytes[FOURTH] = '\0';
 	} else {
 		glyph->bytes[THIRD] = data[FIRST]; 
 		glyph->bytes[FOURTH] = data[SECOND];
 	}
+
 	glyph->end = end;
 
 	return glyph;
@@ -171,8 +175,9 @@ void parse_args(int argc, char** argv){
 }
 
 void print_help(void) {
-	for(int i = 0; i < 4; i++){
-		printf("%s", USAGE[i]); 
+	int j = 0;
+	for(j = 0; j < 4; j++){
+		printf("%s", USAGE[j]); 
 	}
 	quit_converter(NO_FD);
 }
